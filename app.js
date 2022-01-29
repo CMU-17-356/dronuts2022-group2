274fd1d6dfe78a6e145cec2a92dcf8541ed0be32
Alex Li <7alex7li@gmail.com>
2022-01-29 14:31:54 -0500

    Tests pass and server works

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
 const express = require('express')
-const app = express()
 const port = 3000
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+// import { hello } from "hello";
+const app = require('./hello');
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+module.exports = app
