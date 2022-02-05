ef4343e02ca4c834f7e37764f5ad413f1617fd42
ykyle01 <47197987+ykyle01@users.noreply.github.com>
2022-02-05 15:17:17 -0500

    Changed linter rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,5 +20,6 @@ module.exports = {
     '@typescript-eslint',
   ],
   'rules': {
+    'linebreak-style': 0,
   },
 };
--- hello.ts
+++ hello.ts
@@ -3,6 +3,6 @@ const app = express();
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
-})
+});
 
 module.exports = app;
