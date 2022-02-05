43804fa88b0a948a23a3ab3a03e7194daeafec35
ykyle01 <47197987+ykyle01@users.noreply.github.com>
2022-02-05 15:42:59 -0500

    Tidy test code

--- hello.test.ts
+++ hello.test.ts
@@ -1,9 +1,9 @@
-const request = require("supertest");
-const app = require('./hello');
+const request = require('supertest');
+const helloApp = require('./hello');
 
-describe("Testing hello world works", () => {
-    test("hello world gives status 200", async () => {
-      await request(app).get("/").expect(200);
-    })
-})
+describe('Testing hello world works', () => {
+  test('hello world gives status 200', async () => {
+    await request(helloApp).get('/').expect(200);
+  });
+});
 
