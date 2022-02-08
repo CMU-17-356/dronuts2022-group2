459bb2f1b1e7daa126a8a322aee35c0596c49365
ykyle01 <47197987+ykyle01@users.noreply.github.com>
2022-02-07 23:38:38 -0500

    Added navbar

--- frontend/src/components/NavBar/NavBar.tsx
+++ frontend/src/components/NavBar/NavBar.tsx
@@ -1,11 +1,20 @@
 import React, { FC } from 'react';
 import './NavBar.css';
+import { Navbar, Container, Nav } from 'react-bootstrap';
 
 interface NavBarProps {}
 
 const NavBar: FC<NavBarProps> = () => (
   <div className="NavBar">
-    NavBar Component
+    <Navbar bg="light" variant="light">
+        <Container>
+        <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Checkout</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+        </Nav>
+        </Container>
+    </Navbar>
   </div>
 );
 
