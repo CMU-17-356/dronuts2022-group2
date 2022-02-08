eb2f105c605d3c74fc58344e6c7122a47db91ef1
Sam Flattery <sflatter@andrew.cmu.edu>
2022-02-07 20:47:37 -0500

    add drone schema

---
+++ backend/schemas/drone_schema.test.ts
@@ -0,0 +1,20 @@
+import { droneSchema, Drone } from './drone_schema';
+import { Error } from 'mongoose';
+
+test("Drone ID required field", done => {
+    var drone = new Drone();
+
+    drone.validate(function(err : any) {
+        expect(err.errors.id).toBeDefined();
+        done();
+    });
+});
+
+test("Drone charge validated", done => {
+    var drone = new Drone({id: 1, charge: 3});
+
+    drone.validate(function(err : any) {
+        expect(err.errors.charge).toBeDefined();
+        done();
+    });
+});
