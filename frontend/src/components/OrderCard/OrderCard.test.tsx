5c247a090a93c025c3de6d4784921f7b747d0183
Alex Li <7Alex7Li@gmail.com>
2022-02-12 20:48:20 -0500

    Make backlog without destroying git history (#31)

--- frontend/src/components/OrderCard/OrderCard.test.tsx
+++ frontend/src/components/OrderCard/OrderCard.test.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import DroneCard from './DroneCard';
-
+import OrderCard from './OrderCard';
+import { order_1 } from "../../sample_data";
 
 it('should mount', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<DroneCard/>, div);
+  ReactDOM.render(<OrderCard  order={order_1}/>, div);
   ReactDOM.unmountComponentAtNode(div);
 });
