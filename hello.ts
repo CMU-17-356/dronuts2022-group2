89cd6bd0821d1930778b702833ca59b146575b85
Sam Flattery <sflatter@andrew.cmu.edu>
2022-02-22 09:47:50 -0500

    eslint fix

--- hello.ts
+++ hello.ts
@@ -1,11 +1,11 @@
 import express = require('express');
 import mongoose from 'mongoose';
 import { DonutModel } from './frontend/database/schemas/donut_schema';
-import 'dotenv/config'
+import 'dotenv/config';
 
 const app = express();
 
-mongoose.connect("mongodb+srv://dronuts:" + process.env.MONGO_PASSWORD + "@cluster0.qbzmg.mongodb.net/dronutsDB?retryWrites=true&w=majority");
+mongoose.connect('mongodb+srv://dronuts:' + process.env.MONGO_PASSWORD + '@cluster0.qbzmg.mongodb.net/dronutsDB?retryWrites=true&w=majority');
 
 app.get('/', (req:express.Request, res:express.Response) => {
   res.send('Hello World!');
@@ -13,41 +13,41 @@ app.get('/', (req:express.Request, res:express.Response) => {
 
 app.post('/test', (req, res) => {
   const donut = new DonutModel({
-    name: "Chocolate",
+    name: 'Chocolate',
     price: 1.69,
-    description: "Chocolate donut",
-  })
+    description: 'Chocolate donut',
+  });
 
   donut
-    .save()
-    .then((result: any) => {
-      console.log(result);
-      res.status(201).json({
-        message: "Handling POST requests to /products",
-        createdProduct: result
+      .save()
+      .then((result: any) => {
+        console.log(result);
+        res.status(201).json({
+          message: 'Handling POST requests to /products',
+          createdProduct: result,
+        });
+      })
+      .catch((err: any) => {
+        console.log(err);
+        res.status(500).json({
+          error: err,
+        });
       });
-    })
-    .catch((err: any) => {
-      console.log(err);
-      res.status(500).json({
-        error: err
-      });
-    });
 });
 
 app.get('/test', (req:express.Request, res:express.Response) => {
   DonutModel.find()
-    .exec()
-    .then((docs : any) => {
-      console.log(docs);
-      res.status(200).json(docs);
-    })
-    .catch((err : any) => {
-      console.log(err);
-      res.status(500).json({
-        error: err
+      .exec()
+      .then((docs : any) => {
+        console.log(docs);
+        res.status(200).json(docs);
+      })
+      .catch((err : any) => {
+        console.log(err);
+        res.status(500).json({
+          error: err,
+        });
       });
-    });
 });
 
 export default app;
--- makeDB.ts
+++ makeDB.ts
@@ -3,9 +3,9 @@ import 'dotenv/config';
 import fs from 'fs';
 import { DonutModel } from './frontend/database/schemas/donut_schema';
 
-mongoose.connect("mongodb+srv://dronuts:" + process.env.MONGO_PASSWORD + "@cluster0.qbzmg.mongodb.net/dronutsDB?retryWrites=true&w=majority");
+mongoose.connect('mongodb+srv://dronuts:' + process.env.MONGO_PASSWORD + '@cluster0.qbzmg.mongodb.net/dronutsDB?retryWrites=true&w=majority');
 
-const donutPrice = 1.69
+const donutPrice = 1.69;
 const imageDir = './frontend/src/assets/';
 
 // Delete all donuts so we don't get duplicates.
@@ -17,17 +17,17 @@ function addFiles() {
     files.forEach((fileName, i) => {
       // Reformat the file names from donutName.jpg to 'donut name' and
       // 'Donut Name'.
-      const removeUnderscore = fileName.replace(/_/g, " ");
+      const removeUnderscore = fileName.replace(/_/g, ' ');
       const noJpg = removeUnderscore.slice(0, removeUnderscore.length - 4);
-      const capitalized = noJpg.split(" ").map((word) => {
-          return word[0].toUpperCase() + word.substring(1);
-      }).join(" ");
+      const capitalized = noJpg.split(' ').map((word) => {
+        return word[0].toUpperCase() + word.substring(1);
+      }).join(' ');
       const donut = new DonutModel({
         name: capitalized,
         price: donutPrice,
         imageurl: imageDir + fileName,
-        description: "A nice " + noJpg + " donut"
-      })
+        description: 'A nice ' + noJpg + ' donut',
+      });
       // Save all donuts to the database and close the connection when the last
       // finishes.
       donut.save().then((value) => {
