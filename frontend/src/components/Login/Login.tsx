9035efe06c2bc29932bff509638d17cdd0415494
ykyle01 <47197987+ykyle01@users.noreply.github.com>
2022-02-22 14:13:15 -0500

    Added login component (#39)
    * Added login component
    
    * Updated modules
    
    * Fixing autorefresh issue

--- frontend/src/components/Login/Login.tsx
+++ frontend/src/components/Login/Login.tsx
@@ -1,11 +1,25 @@
 import React, { FC } from 'react';
 import './Login.css';
+import { Button, Form } from 'react-bootstrap';
 
 interface LoginProps {}
 
 const Login: FC<LoginProps> = () => (
   <div className="Login">
-    Login Component
+    <Form>
+        <h1>Login</h1>
+        <Form.Group className="mb-3" controlId="username">
+            <Form.Label>Username</Form.Label>
+            <Form.Control type="username" placeholder="Enter username" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="password">
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password" placeholder="Enter password" />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+            Submit
+        </Button>
+    </Form>
   </div>
 );
 
