233011838d648417e1ce4def73fa57cc3291bce5
ykyle01 <47197987+ykyle01@users.noreply.github.com>
2022-03-01 12:37:43 -0500

    Fixed path issue

--- frontend/src/components/OrderCard/OrderCard.tsx
+++ frontend/src/components/OrderCard/OrderCard.tsx
@@ -5,35 +5,41 @@ import './OrderCard.css';
 
 interface OrderCardProps {order: Order}
 
-const OrderCard: FC<OrderCardProps> = (props) => (
-  <div className="OrderCard">
-    <Accordion.Body>
-      <Container>
-        <Row>
-        <Col xs={10}>
-      {props.order.donuts.map((donut, ind) => (
-        <Row key={ind}>
-          <Col xs={4}>
-            <Image src={String(donut.imageurl)} />
-          </Col>
-          <Col xs={4}>
-              <h3>{donut.name}</h3>
-          </Col>
-          <Col xs={4}>
-            <p>x{props.order.quantities[ind]}</p>
-          </Col>
-          </Row>
-      ))}
+const OrderCard: FC<OrderCardProps> = (props) => {
+    const assignDrone = () => {
+        console.log("HI");
+    }
+
+    return (
+    <div className="OrderCard">
+        <Accordion.Body>
+        <Container>
+            <Row>
+            <Col xs={10}>
+        {props.order.donuts.map((donut, ind) => (
+            <Row key={ind}>
+            <Col xs={4}>
+                <Image src={String(donut.imageurl)} />
+            </Col>
+            <Col xs={4}>
+                <h3>{donut.name}</h3>
+            </Col>
+            <Col xs={4}>
+                <p>x{props.order.quantities[ind]}</p>
+            </Col>
+            </Row>
+        ))}
+            </Col>
+        <Col xs={2}>
+            <div onClick={e=>e.stopPropagation()}>
+                <Button onClick={assignDrone}>Send to drone</Button>
+            </div>
         </Col>
-      <Col xs={2}>
-        <div onClick={e=>e.stopPropagation()}>
-          <Button>Send to drone</Button>
-        </div>
-          </Col>
-          </Row>
-    </Container>
-    </Accordion.Body>
-  </div>
-);
+        </Row>
+        </Container>
+        </Accordion.Body>
+    </div>
+    )
+};
 
 export default OrderCard;
